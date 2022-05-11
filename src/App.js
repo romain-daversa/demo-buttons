649f115cc93e8fd3d95d9138c1c4a586168fbe8f
Romain d'Aversa <69752291+romain-daversa@users.noreply.github.com>
2022-05-11 13:58:36 +0200

    working v1

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import {
   CardContent,
   Typography
 } from "@mui/material";
-import { FindInPageIcon, AnalyticsIcon } from "@mui/icons-material";
+import ArticleIcon from "@mui/icons-material/Article";
+import FindInPageIcon from "@mui/icons-material/FindInPage";
+import AnalyticsIcon from "@mui/icons-material/Analytics";
+import GroupsIcon from "@mui/icons-material/Groups";
+import StarsIcon from "@mui/icons-material/Stars";
+import WebIcon from "@mui/icons-material/Web";
+import AdminPanelSettingsIcon from "@mui/icons-material/AdminPanelSettings";
+import SchoolIcon from "@mui/icons-material/School";
 
 const data = [
   {
     text: "SharePoint Initiative Offer",
     color: "#008e5c",
-    icon: <FindInPageIcon fontSize="large" />
+    icon: <ArticleIcon fontSize="large" />
   },
   {
     text: "New Site Request Form",
@@ -27,53 +34,68 @@ const data = [
   {
     text: "Access Management Model",
     color: "#878787",
-    icon: <FindInPageIcon fontSize="large" />
+    icon: <GroupsIcon fontSize="large" />
   },
   {
     text: "Compliance Style Charter",
     color: "#b4c832",
-    icon: <FindInPageIcon fontSize="large" />
+    icon: <StarsIcon fontSize="large" />
   },
   {
     text: "Owners SP Golden RUles",
     color: "#e9b538",
-    icon: <FindInPageIcon fontSize="large" />
+    icon: <WebIcon fontSize="large" />
   },
   {
     text: "Confidentiality Level",
     color: "#f39476",
-    icon: <FindInPageIcon fontSize="large" />
+    icon: <AdminPanelSettingsIcon fontSize="large" />
   },
   {
     text: "Digital Working SP Training",
     color: "#887d95",
-    icon: <FindInPageIcon fontSize="large" />
+    icon: <SchoolIcon fontSize="large" />
   }
 ];
 
 export default function App() {
   return (
-    <div className="App">
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={1}
-      >
-        {data.map((row) => (
-          <Grid xs={3} item key={row.text}>
-            <Card sx={{ background: row.color, color: "white" }}>
-              <CardActionArea>
-                <CardContent>
-                  {row.icon}
-                  <Typography color="">{row.text}</Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
+    <Grid
+      container
+      spacing={0}
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid item xs={10}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={1}
+        >
+          {data.map((row) => (
+            <Grid item sm={3} key={row.text}>
+              <Card
+                sx={{
+                  background: row.color,
+                  color: "white",
+                  height: "100%",
+                  textAlign: "center"
+                }}
+              >
+                <CardActionArea>
+                  <CardContent>
+                    {row.icon}
+                    <Typography>{row.text}</Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Grid>
-    </div>
+    </Grid>
   );
 }
